df4a36f66525976de4dd081cc173cba4f5051730
MusawerAli <pakjalihouse@gmail.com>
2019-09-26 12:55:17 +0100

    test

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -133,7 +133,7 @@ class LoginController extends CI_Controller {
 	$target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+echo $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($submit)) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
